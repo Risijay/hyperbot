5efbac8ad561b42cf08eb1781fbf21e5de93a7cf
Risijay <65485008+Risijay@users.noreply.github.com>
2021-04-18 16:05:47 +0530

    Added MongoDB_URI and PGN_URL

--- serve.sh
+++ serve.sh
@@ -4,5 +4,8 @@ export TOKEN=
 export BOT_NAME=
 export USE_LC0=true
 export ENGINE_MOVE_OVERHEAD=1000
+export MONGODB_URI=mongodb+srv://risith:sagara12@cluster0.bv7tb.mongodb.net
+export PGN_URL=https://ccrl.chessdom.com/ccrl/4040/games-by-month/2021-01.bare.[10557].pgn.7z
+export MAX_GAMES= 100000
 
 node server.js
